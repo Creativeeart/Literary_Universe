1d22ff5a387aafeb82c90abd2d1b468763927cd0
Rinat_Home_DesktopPC <Masterweb34@yandex.ru>
2020-07-05 23:46:19 +0500

    Хоп

--- Assets/Scripts/AudioManager/Sound.cs
+++ Assets/Scripts/AudioManager/Sound.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 [System.Serializable]
-public class Sound {
+public class Sound{
 	public string name;
 	public GameObject parent;
 	public AudioClip clip;
